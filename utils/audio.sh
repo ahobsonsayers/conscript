769b9805a40915376c3d47ff0f92ac80e9cc2410
Arran Hobson Sayers <ahobsonsayers@gmail.com>
2024-04-28 20:35:39 +0100

    Move some utils around

---
+++ utils/audio.sh
@@ -0,0 +1,157 @@
+#!/usr/bin/env bash
+
+function video_duration {
+  media_duration audio "$@"
+}
+
+function video_duration_seconds {
+  media_duration_seconds audio "$@"
+}
+
+function audio_quality {
+  if [[ $# -ne 2 ]]; then
+    echo "Usage: ${FUNCNAME[0]} <source> <target>"
+    return 1
+  fi
+
+  local source_dir
+  local target_dir
+  source_dir="$(dirname "$1")"
+  target_dir="$(dirname "$2")"
+
+  local source_label
+  local target_label
+  source_label="$(file_label "$1")"
+  target_label="$(file_label "$2")"
+
+  local source_wav="${source_dir}/${source_label}.wav"
+  local target_wav="${target_dir}/${target_label}.wav"
+  local output_json="${target_dir}/${target_label}.json"
+
+  # Convert source and target audio to wav (if required)
+  if [[ ! -f $source_wav ]]; then
+    echo "Converting $1 audio to wav"
+    ffmpeg \
+      -hide_banner -v warning \
+      -nostdin -stats \
+      -i "$1" \
+      -map a:0 \
+      -codec:a pcm_s16le \
+      -ac 2 \
+      -ar 48000 \
+      "$source_wav" ||
+      return 1
+    echo
+  fi
+
+  if [[ ! -f $target_wav ]]; then
+    echo "Converting $2 audio to wav"
+    ffmpeg \
+      -hide_banner -v warning \
+      -nostdin -stats \
+      -i "$2" \
+      -map a:0 \
+      -codec:a pcm_s16le \
+      -ac 2 \
+      -ar 48000 \
+      "$target_wav" ||
+      return 1
+    echo
+  fi
+
+  # Check if files are out of sync
+  local diff_result
+  local diff_file
+  local diff_time
+  diff_result="$(audio_diff "$source_wav" "$target_wav")"
+  diff_file="$(cut -d " " -f 1 <<<"$diff_result")"
+  diff_time="$(cut -d " " -f 2 <<<"$diff_result")"
+
+  # Sync files if required
+  if [[ $diff_time != 0 ]]; then
+
+    local diff_dir
+    diff_dir="$(dirname "$diff_file")"
+
+    local diff_label
+    diff_label="$(file_label "$diff_file")"
+
+    local sync_file="${diff_dir}/sync-${diff_label}.wav"
+
+    echo "Trimming $diff_file by ${diff_time}s to sync audio"
+    ffmpeg \
+      -hide_banner -v warning \
+      -nostdin -stats \
+      -i "$diff_file" \
+      -ss "$diff_time" \
+      "$sync_file" ||
+      return 1
+    echo
+
+    mv "$sync_file" "$diff_file"
+  fi
+
+  echo "Measuring audio quality"
+  local results
+  results="$(
+    peaqb \
+      -r "$source_wav" \
+      -t "$target_wav" |
+      grep "ODG:" |
+      cut -d " " -f 2
+  )"
+  if [[ -z $results ]]; then
+    return 1
+  fi
+
+  local mean
+  local median
+  local stddev
+  local min
+  local max
+
+  mean="$(st -q --mean <<<"$results")"
+  median="$(st -q --median <<<"$results")"
+  stddev="$(st -q --stddev <<<"$results")"
+  min="$(st -q --min <<<"$results")"
+  max="$(st -q --max <<<"$results")"
+
+  jq -n \
+    --argjson mean "$mean" \
+    --argjson median "$median" \
+    --argjson stddev "$stddev" \
+    --argjson min "$min" \
+    --argjson max "$max" \
+    '{
+      "mean": $mean,
+      "median": $median,
+      "stddev": $stddev,
+      "min": $min,
+      "max": $max
+    }' \
+    >"$output_json"
+
+  echo "Written results to $output_json"
+}
+
+function audio_diff() {
+  if [[ $# -ne 2 ]]; then
+    echo "Usage: ${FUNCNAME[0]} <source> <target>"
+    return 1
+  fi
+
+  local result
+  result="$(
+    syncstart "$1" "$2" -st 120 \
+      2>&1 |
+      tail -n 1 |
+      sed "s|[\(\)',]||g"
+  )"
+
+  local filename
+  local time
+  filename="$(cut -d " " -f 1 <<<"$result")"
+  time="$(cut -d " " -f 2 <<<"$result")"
+
+  printf "%s %g\n" "$filename" "$time"
+}
---
+++ utils/video.sh
@@ -0,0 +1,94 @@
+#!/usr/bin/env bash
+
+function video_duration {
+  media_duration video "$@"
+}
+
+function video_duration_seconds {
+  media_duration_seconds video "$@"
+}
+
+# Get video quality metrics
+# Requires ffmpeg-quality-metrics
+# https://github.com/slhck/ffmpeg-quality-metrics
+# pipx install ffmpeg-quality-metrics
+function video_quality {
+  if [[ $# -ne 2 ]]; then
+    echo "Usage: ${FUNCNAME[0]} <source> <target>"
+    return 1
+  fi
+
+  local output_dir
+  output_dir="$(dirname "$2")"
+
+  local output_label
+  output_label="$(file_label "$2")"
+
+  local output_json="${output_dir}/${output_label}.json"
+
+  echo "Measuring video quality"
+
+  ffmpeg-quality-metrics "$2" "$1" \
+    -s lanczos \
+    -m vmaf \
+    --vmaf-features float_ms_ssim psnr_hvs |
+    jq '.global | {
+       vmaf: .vmaf.vmaf,
+       ssim: .vmaf.float_ms_ssim,
+       psnr: .vmaf.psnr_hvs
+    }' >"$output_json"
+
+  # ffmpeg-quality-metrics "$2" "$1" \
+  #   -s lanczos \
+  #   -m vmaf ssim psnr |
+  #   jq '.global | {
+  #      vmaf: .vmaf.vmaf,
+  #      ssim: .ssim.ssim_avg,
+  #      psnr: .psnr.psnr_avg
+  #   }' > "$output_json"
+
+  echo "Written results to $output_json"
+}
+
+function video_crop_lossless {
+  check_installed ffmpeg
+
+  if [[ $# -ne 2 ]]; then
+    echo "Usage: ${FUNCNAME[0]} <input> <output>"
+    return 1
+  fi
+
+  local source_height
+  source_height="$(ffheight "$1")" || return 1
+  echo "Video Height: $source_height"
+
+  local target_height
+  target_height=$(ffcropheight "$1") || return 1
+
+  local crop_multiple
+  crop_multiple=6
+
+  # Get source crop height as a multiple of a
+  # number depending on initial resolution
+  local target_height_multiple
+  target_height_multiple=$(((target_height + crop_multiple - 1) / crop_multiple * crop_multiple)) || return 1
+
+  if [[ $target_height_multiple -lt $source_height ]]; then
+    echo "Crop Height: $target_height_multiple"
+  else
+    echo "No crop required. Skipping"
+    exit 0
+  fi
+
+  ffmpeg \
+    -hide_banner -v warning \
+    -nostdin -stats \
+    -i "$1" \
+    -c:v libx264 \
+    -preset ultrafast \
+    -qp 0 \
+    -vf "crop=iw:$target_height_multiple" \
+    -c:a copy \
+    -c:s copy \
+    -y "$2"
+}
