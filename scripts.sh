5b886e3a6a62b1f6b62efbe1cae00f28d2b878b8
Arran Hobson Sayers <ahobsonsayers@gmail.com>
2024-05-11 11:35:01 +0100

    Update some scripts

--- scripts.sh
+++ scripts.sh
@@ -1,10 +1,9 @@
 #!/usr/bin/env bash
-# shellcheck disable=SC1090
 
 CURRENT_FILE="${BASH_SOURCE[0]}"
 SCRIPTS_DIR=$(cd -- "$(dirname -- "$CURRENT_FILE")" &>/dev/null && pwd)
 
 export PATH="$SCRIPTS_DIR/bin:$PATH"
-export BASH_UTILS="$SCRIPTS_DIR/utils"
+export SHELL_UTILS="$SCRIPTS_DIR/utils"
 
-source "$BASH_UTILS/utils.sh"
+source "$SHELL_UTILS/utils.sh"
--- utils/utils.sh
+++ utils/utils.sh
@@ -2,9 +2,9 @@
 # shellcheck disable=SC1090
 
 CURRENT_FILE="${BASH_SOURCE[0]}"
-BASH_UTILS=$(cd -- "$(dirname -- "$CURRENT_FILE")" &>/dev/null && pwd)
+SHELL_UTILS=$(cd -- "$(dirname -- "$CURRENT_FILE")" &>/dev/null && pwd)
 
-for file in "$BASH_UTILS"/*.sh; do
+for file in "$SHELL_UTILS"/*.sh; do
   if [[ $file != "$CURRENT_FILE" && -r $file ]]; then
     source "$file"
   fi
